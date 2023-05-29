11a89a12a61eca5b6bac7549ac2f3c60026c45ac
rt <randytyng21@gmail.com>
2023-05-29 17:08:53 -0400

    Setup rotating cube
    setup rotating cube but it won't work for some reason. Will come back to it later after a break

--- jscsshtml/main.js
+++ jscsshtml/main.js
@@ -1,17 +1,44 @@
+// Create scene, camera, and renderer using three.js
+// These will be used to create a rotating 3d cube
 
+var scene = new THREE.scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
 
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
+// Create cube here
 
-document.getElementById('changeColorBtn').addEventListener('click', function() {
-    document.body.style.backgroundColor = getRandomColor();
-});
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var cube = new THREE.Mesh(geometry, material);
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+scene.add(cube);
+camera.position.z = 5;
+
+// Code for animation loop
+
+function animate() {
+    requestAnimationFrame(animate); 
+    renderer.render(scene, camera);
 }
+animate();
+
+// Add an event listener for mouse movement
+
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+// Function for rotation based on mouse position
+
+var mouseX = 0;
+var mouseY = 0;
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - window.innerWidth / 2) / 100;
+    mouseY = (event.clientY - window.innerHeight/ 2) / 100;
+
+    cube.rotation.x += mouseY;
+    cube.rotation.y += mouseX;
+}
+
 

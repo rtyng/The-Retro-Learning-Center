2768d6fb40481ce36ab427a7c4f9415fd7b8fe9d
rt <randytyng21@gmail.com>
2023-05-30 00:58:51 -0400

    Used npm to set up environment and installed express.js

--- jscsshtml/main.js
+++ jscsshtml/main.js
@@ -1,22 +1,24 @@
-import * as THREE from '..\\node_modules';
+import * as THREE from 'three';
 
 // Create scene, camera, and renderer using three.js
 // These will be used to create a rotating 3d cube
+ 
+const scene = new THREE.Scene();
 
-var scene = new THREE.scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
+// Attriubtes are FOV, aspect ratio, near and far "clipping" planes for last 2 
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // Multiple types of cameras in three
+const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement) ;
 
 // Create cube here
 
 var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
+var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+var cube = new THREE.Mesh( geometry, material );
 
-scene.add(cube);
+scene.add( cube );
 camera.position.z = 5;
 
 // Code for animation loop
